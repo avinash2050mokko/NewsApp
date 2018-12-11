6edcf29ccd6f4e09414ca9f1d47684a34ebdfee1
Avinash kumar <avinash2050kumar@gmail.com>
2018-12-11 08:54:54 +0530

    [ FIX ] Added Description section to the News Card

--- src/component/NewsCard.js
+++ src/component/NewsCard.js
@@ -1,18 +1,25 @@
 import React from 'react'
-import { Image, Text, View } from 'react-native'
+import {
+	Image,
+	Text,
+	LayoutAnimation,
+	Platform,
+	UIManager,
+	View
+} from 'react-native'
 import styled from 'styled-components'
 import moment from 'moment'
 
 const ScreenArea = styled.View`
 	align-items: center;
-	background-color: #fff;
+	background-color: #2a2d36;
 	border-bottom-width: 1px;
-	border-bottom-color: #efefef;
+	border-bottom-color: #44464d;
 	padding-top: 15px;
 	padding-bottom: 15px;
 `
 
-const Area = styled.View`
+const Area = styled.TouchableOpacity`
 	flex-direction: row;
 `
 
@@ -24,19 +31,49 @@ const TimeArea = styled.View`
 `
 
 const CategoryLabel = styled.Text`
-	color: #04000b;
+	color: #fff;
 	flex: 1;
 	flex-wrap: wrap;
 	font-size: 14px;
 `
 
 export default class NewsCard extends React.Component {
+	constructor() {
+		super()
+
+		this.state = {
+			onLayoutHeight: 0,
+			modifiedHeight: 0,
+			expanded: false,
+			checked: 0
+		}
+
+		if (Platform.OS === 'android') {
+			UIManager.setLayoutAnimationEnabledExperimental(true)
+		}
+	}
+
+	changeLayout = () => {
+		LayoutAnimation.configureNext(LayoutAnimation.Presets.spring)
+
+		if (this.state.expanded === false)
+			this.setState({
+				modifiedHeight: this.state.onLayoutHeight,
+				expanded: true
+			})
+		else this.setState({ modifiedHeight: 0, expanded: false })
+	}
+
+	getViewHeight(height) {
+		this.setState({ onLayoutHeight: height })
+	}
+
 	render() {
-		const { title, imageUrl, time } = this.props
+		const { title, imageUrl, time, description, content } = this.props
 
 		return (
 			<ScreenArea>
-				<Area>
+				<Area onPress={this.changeLayout}>
 					<Image
 						style={{
 							width: imageUrl === null ? 0 : 90,
@@ -47,10 +84,29 @@ export default class NewsCard extends React.Component {
 					/>
 					<CategoryLabel>{title ? title : 'No Title'}</CategoryLabel>
 				</Area>
+
+				{/*Expanded data */}
+				<View
+					style={{
+						height: this.state.modifiedHeight,
+						overflow: 'hidden',
+						flexWrap: 'wrap'
+					}}
+					onLayout={event => this.getViewHeight(null)}
+				>
+					<Text
+						style={{ color: '#ccc', fontSize: 12, marginTop: 20 }}
+					>
+						{description}
+					</Text>
+				</View>
+
 				<TimeArea>
 					<Text
 						style={{
-							color: '#FF6666',
+							color: '#2bc5b4',
+							marginLeft: 5,
+							marginTop: 3,
 							fontSize: 10
 						}}
 					>
